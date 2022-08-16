6627aa9de87f8ee515bed34117847c1225e230b0
Arch0wl <annstarovoitov@gmail.com>
2022-08-16 15:15:09 -0400

    styles

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react"
-import Navbar from "./components/Navbar"
-// import Hero from "./components/Hero"
-import Card from "./components/Card"
+import Navbar from "./components/Navbar.js"
+import Hero from "./components/Hero.js"
+import Card from "./components/Card.js"
 
 /*
 Challenge:
@@ -16,7 +16,7 @@ still be block elements, stacked vertically. We'll add styling later.
 */
 
 export default function App() {
-            // <Hero />
+            <Hero />
     return (
         <div>
             <Navbar />
